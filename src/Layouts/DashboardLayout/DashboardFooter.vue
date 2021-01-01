20e30fff54c37f1e15f6f839423dc5060bf96498
Huzaifa Rasheed <huzaifarasheed29@gmail.com>
2021-01-01 16:42:22 +0500

    Updated Detail page with cards

--- src/Layouts/DashboardLayout/DashboardFooter.vue
+++ src/Layouts/DashboardLayout/DashboardFooter.vue
@@ -1,27 +1,30 @@
 <template>
-    <div>
-          <div class="row text-center no-gutters border-blue text-grey pt-1 mt-2" style="font-size: 12px;">
-            <div class="col-md-4 mt-2">
-                <p class="mb-0">Copyright © 2020</p>
-                <p class="mb-0">Powered by AMSUS Technologies</p>
-                <p>Privacy Policy</p>
-            </div>
-            <div class="col-md-4 mt-2">
-                <p class="">Our Mailing Address</p>
-                <p>Contact Details</p>
-            </div>
-            <div class="col-md-4 p-3 rounded-icon mt-2">
-                <img class="mx-1" src="img\Group 23.png" alt="">
-                <img class="mx-1" src="img\Group 22.png" alt="">
-                <img class="mx-1" src="img\Group 25.png" alt="">
-                <img class="mx-1" src="img\Group 24.png" alt="">
-            </div>
-        </div>
-    </div>    
+  <div>
+    <div
+      class="row text-center no-gutters border-blue text-grey pt-1 mt-2"
+      style="font-size: 12px"
+    >
+      <div class="col-md-4 mt-2">
+        <p class="mb-0">Copyright © 2020</p>
+        <p class="mb-0">Powered by AMSUS Technologies</p>
+        <p>Privacy Policy</p>
+      </div>
+      <div class="col-md-4 mt-2">
+        <p class="">Our Mailing Address</p>
+        <p>Contact Details</p>
+      </div>
+      <div class="col-md-4 rounded-icon">
+        <img class="mx-1" src="@/assets/images/others/Group 23.png" alt="" />
+        <img class="mx-1" src="@/assets/images/others/Group 22.png" alt="" />
+        <img class="mx-1" src="@/assets/images/others/Group 25.png" alt="" />
+        <img class="mx-1" src="@/assets/images/others/Group 24.png" alt="" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'DashboardFooter'
-}
+  name: "DashboardFooter",
+};
 </script>
